ced9bdbe77880fb866fe2f926d5143b74baa29a3
alaa shoukri <alaaelsweedy12@gmail.com>
2020-05-12 21:35:53 +0200

    add headline news

---
+++ lib/screens/headline_news.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/home_tabs/favourite.dart';
+import 'package:news_app/home_tabs/popular.dart';
+import 'package:news_app/home_tabs/whatsnew.dart';
+import 'package:news_app/shared_ui/drawer.dart';
+
+
+class HeadlineNews extends StatefulWidget {
+  @override
+  _HeadlineNewsState createState() => _HeadlineNewsState();
+}
+
+class _HeadlineNewsState extends State<HeadlineNews> with SingleTickerProviderStateMixin{
+
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(initialIndex: 0, length: 3, vsync: this);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: NavigationDrawer(),
+      appBar: AppBar(
+        title: Text('Headline News'),
+        bottom: TabBar(
+          indicatorColor: Colors.white,
+          tabs: [
+            Tab(
+              text: "WHAT'S NEW",
+            ),
+            Tab(
+              text: "POPULAR",
+            ),
+            Tab(
+              text: "FAVOURITE",
+            ),
+          ],
+          controller: _tabController,
+        ),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.search), onPressed: () {}),
+          IconButton(
+            icon: Icon(Icons.more_vert),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Center(
+        child: TabBarView(
+          children: [FaVOURITE(), POPULAR(), FaVOURITE()],
+          controller: _tabController,
+        ),
+      ),
+    );
+  }
+}
--- lib/shared_ui/drawer.dart
+++ lib/shared_ui/drawer.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/models/nav_menu_item.dart';
+import 'package:news_app/screens/home_screen.dart';
+import 'package:news_app/screens/headline_news.dart';
 
 class NavigationDrawer extends StatefulWidget {
   @override
@@ -6,9 +9,46 @@ class NavigationDrawer extends StatefulWidget {
 }
 
 class _NavigationDrawerState extends State<NavigationDrawer> {
+
+  List<NavMenuItem> navigationItem = [
+    NavMenuItem('Explore', () => HomeScreen()),
+    NavMenuItem('Headlines', () => HeadlineNews())
+  ];
+
+//  List<String> _namesDrawer = [
+//    'Explore',
+//    'Heading News',
+//    'Read Later',
+//    'Videos',
+//    'Photos',
+//    'Setting',
+//    'Logout'
+//  ];
+
   @override
   Widget build(BuildContext context) {
-    return Drawer();
+    return Drawer(
+      child: Padding(
+        padding: const EdgeInsets.only(top: 40, left: 16),
+        child: ListView.builder(
+          itemBuilder: (context, position) {
+            return ListTile(
+              title: Text(navigationItem[position].title),
+              trailing: Icon(Icons.chevron_right),
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return navigationItem[position].destination();
+                    },
+                  ),
+                );
+              },
+            );
+          },
+          itemCount: navigationItem.length,
+        ),
+      ),
+    );
   }
 }
-
